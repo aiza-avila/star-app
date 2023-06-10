2bf1ac0c48367ec20bcdb2d4b0dc3aad8661c670
aiza-avila <b00798654@essec.edu>
2023-06-10 16:38:53 +0200

    Update deploy_picture_star.py

--- deploy_picture_star.py
+++ deploy_picture_star.py
@@ -120,7 +120,7 @@ def main():
         # Display the results
         st.write("Results:")
         st.write(calif_final)
-        st.write(predicted_values['score']))
+        st.write(predicted_values['score'])
 
 
 # Run the app
